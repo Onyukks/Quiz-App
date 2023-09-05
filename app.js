5a441a66347e278874609c21cad482c1a730b24c
Onyeukwu <onyeukwuagbafo@gmail.com>
2023-09-05 22:18:10 +0100

    Updating the questions

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const correctAnswers = ['B','C','B','A'];
+const correctAnswers = ['C','B','B','C','A'];
 const form = document.querySelector('.quiz-form')
 const result = document.querySelector('.result')
 form.addEventListener('submit',e=>{
     e.preventDefault()
     let score = 0;
-    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value];
+    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value];
     userAnswers.forEach((answers,index)=>{
         if(answers===correctAnswers[index]){
-            score+=25;
+            score+=20;
         }
     })
     scrollTo(0,0)
